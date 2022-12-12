d4d658815637f531a07c0fcf70830c01fe372014
Cian Comerford <cian.comerford5@mail.dcu.ie>
2022-12-12 23:39:47 +0000

    Navbar bug fix

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rme-=a0xauy4m#h_s05!#l5+n*i*@l^$a!73927ktu&utc@k&d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
