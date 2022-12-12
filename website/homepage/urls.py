d8cf4fca15f345b8e35e0a8d8a561e93b5c21146
Cian Comerford <cian.comerford5@mail.dcu.ie>
2022-12-12 21:17:20 +0000

    About and contact done

--- website/homepage/urls.py
+++ website/homepage/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path("about/", views.about, name='about'),
+    path("contact/", views.contact, name='contact'),
+    path("product1/", views.product1, name='product1'),
+    path("product2/", views.product2, name='product2'),
+    path("product3/", views.product3, name='product3'),
+    path("product4/", views.product4, name='product4'),
 ]
--- website/homepage/views.py
+++ website/homepage/views.py
@@ -4,3 +4,27 @@ from django.http import HttpResponse
 def index(request):
     context = {}
     return render(request, "homepage/index.html", context)
+
+def about(request):
+    context = {}
+    return render(request, "homepage/about.html", context)
+
+def contact(request):
+    context = {}
+    return render(request, "homepage/contact.html", context)
+
+def product1(request):
+    context = {}
+    return render(request, "homepage/product_1.html", context)
+
+def product2(request):
+    context = {}
+    return render(request, "homepage/product_2.html", context)
+
+def product3(request):
+    context = {}
+    return render(request, "homepage/product_3.html", context)
+
+def product4(request):
+    context = {}
+    return render(request, "homepage/product_4.html", context)
